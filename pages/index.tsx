826d5ff6c9e98a9a12f9bed379e06f19d5a67e59
@Zylto11 <166717037+Zylto11@users.noreply.github.com>
2025-01-09 19:21:37 +0800

    index.tsx
    added simple indexing

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import AboutSection from "@/components/AboutSection";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-50 to-white">
+      <AboutSection />
+    </div>
+  );
+};
+
+export default Index;
